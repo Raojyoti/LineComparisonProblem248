cd4beaa6d4f308175bdc43930a94d88928cf4ae4
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-21 14:26:33 +0530

    [Jyoti Rao] Calculate length of line

---
+++ LineComparisonProblem/CartesianSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisonProblem
+{
+    public class CartesianSystem
+    {
+        //static variables
+        public static double x1,x2,y1,y2;
+        public static double lengthOfLine;
+        //UC1- Calculate length of line
+        public static void CalculateLengthOfLine()
+        {
+            Console.WriteLine("Please Enter the values for x1 and x2: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the values for y1 and y2: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            lengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of Line: {0}", lengthOfLine);
+            Console.ReadLine();
+        }
+    }
+}
