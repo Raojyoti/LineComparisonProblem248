bbcccf4e4e9634caa3a93b7d452c30ee591cace2
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-21 14:59:45 +0530

    [Jyoti Rao] Modified: Check Equality Of Two Lines

--- LineComparisonProblem/CartesianSystem.cs
+++ LineComparisonProblem/CartesianSystem.cs
@@ -7,8 +7,8 @@ namespace LineComparisonProblem
     public class CartesianSystem
     {
         //static variables
-        public static double x1,x2,y1,y2;
-        public static double lengthOfLine;
+        public static double x1,x2,x3,x4,y1,y2,y3,y4;
+        public static double lengthOfLine, lengthOfLine1, lengthOfLine2;
         //UC1- Calculate length of line
         public static void CalculateLengthOfLine()
         {
@@ -20,7 +20,28 @@ namespace LineComparisonProblem
             y2 = Convert.ToInt32(Console.ReadLine());
             lengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
             Console.WriteLine("Length of Line: {0}", lengthOfLine);
-            Console.ReadLine();
+        }
+        //UC2- Check Equality Of Two Lines
+        public static void CheckEqualityOfTwoLines()
+        {
+            Console.WriteLine("Please Enter the values for x1 and x2: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the values for y1 and y2: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the values for x3 and x4: ");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the values for y3 and y4: ");
+            y3 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+            lengthOfLine1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of Line: {0}", lengthOfLine1);
+            lengthOfLine2 = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            Console.WriteLine("Length of Line: {0}", lengthOfLine2);
+            bool checkEquality=lengthOfLine1.Equals(lengthOfLine2);
+            Console.WriteLine("Equality Of Two Lines: {0}",checkEquality);
         }
     }
 }
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace LineComparisonProblem
 {
@@ -7,7 +8,31 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-            CartesianSystem.CalculateLengthOfLine();
+            while (true)
+            {
+                Console.WriteLine("Select given options");
+                Console.WriteLine("1.CalculateLengthOfLine\n2.CheckEqualityOfTwoLines\n3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        CartesianSystem.CalculateLengthOfLine();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        CartesianSystem.CheckEqualityOfTwoLines();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Select only given options");
+                        break;
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
